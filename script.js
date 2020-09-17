e6b27816a2d4c378be82b1040889889d8edf5eac
Siva <sendmsg2Siva@gmail.com>
2020-09-17 23:43:49 +0530

    style changes and added loader

--- script.js
+++ script.js
@@ -1,179 +1,192 @@
-$(document).ready(function () {
+try {
+  const isFunction = (val) => {
+    return typeof val === "function";
+  };
 
-    const sample_schema = {
-        log: {
-            entries: {
-                _resourceType: true,
-                request: {
-                    method: true,
-                    url: true,
-                    queryString: true,
-                    postData: true
-                },
-                response: {
-                    status: true,
-                    content: {
-                        text: true
-                    }
-                }
-            }
-        }
-    }
+  const isPlainObject = (val) => {
+    return typeof val === "object" && !Array.isArray(val);
+  };
 
-    $("#data").val(JSON.stringify({}, null, 2))
-    $("#schema").val(JSON.stringify(sample_schema, null, 2))
+  const isArray = (val) => {
+    return Array.isArray(val);
+  };
 
-    const isPlainObject = (val) => {
-        return typeof val === 'object' && !Array.isArray(val)
-    }
+  const getValidRoute = (route) => {
+    if (typeof route === "object") return undefined;
+    const validRoute = `${route}`.startsWith("/") ? `${route}` : "/" + route;
+    return validRoute;
+  };
 
-    const isArray = (val) => {
-        return Array.isArray(val)
-    }
+  const isEmpty = (val) => {
+    if (typeof val === "string" && val.length === 0) return true;
+    if (val === null) return true;
+    if (val === undefined) return true;
+    if (typeof val === "object" && !Array.isArray(val) && Object.keys(val).length === 0) return true;
+    if (typeof val === "object" && Array.isArray(val) && val.length === 0) return true;
 
-    const getValidRoute = (route) => {
-        if (typeof route === 'object') return undefined;
-        const validRoute = `${route}`.startsWith("/") ? `${route}` : "/" + route;
-        return validRoute;
-    };
+    return false;
+  };
 
-    const transformHar = (harData = {}, filters = []) => {
+  transformHar = (harData = {}, resourceTypeFilters = [], callback) => {
+    try {
+      const entries = harData?.log?.entries || [];
+      const resourceFilteredEntries = resourceTypeFilters.length ? entries.filter((e) => resourceTypeFilters.indexOf(e._resourceType) >= 0) : entries;
+      const mock = resourceFilteredEntries.reduce((result, entry) => {
+        const route = new URL(entry?.request?.url).pathname;
+        const valid_Route = getValidRoute(route);
+        const responseText = entry?.response?.content?.text || "";
+
+        let response;
         try {
-            const entries = harData.log ? harData.log.entries ? harData.log.entries : [] : [];
-            const xhrFiltered = entries.filter((e) => filters.indexOf(e._resourceType) >= 0);
-            const statusFiltered = xhrFiltered.filter((x) => x.response.status >= 200 && x.response.status < 400);
-
-            const mock = statusFiltered.reduce((result, data) => {
-                const route = new URL(data.request.url).pathname;
-                const valid_Route = getValidRoute(route);
-                const responseText = data.response ?
-                    data.response.content ?
-                    data.response.content.text ?
-                    data.response.content.text : "" : "" : "";
-
-                let response;
-                try {
-                    response = JSON.parse(responseText);
-                } catch {
-                    response = responseText;
-                }
-
-                return {
-                    ...result,
-                    [valid_Route]: response,
-                };
-            }, {});
-
-            return mock;
-        } catch (err) {
-            console.error(err.message);
+          response = JSON.parse(responseText);
+        } catch {
+          response = responseText;
         }
+
+        let obj = { [valid_Route]: response };
+
+        if (isFunction(callback)) {
+          obj = callback(entry, valid_Route, response);
+        }
+
+        return {
+          ...result,
+          ...obj,
+        };
+      }, {});
+
+      const valid_Mock = Object.entries(mock).reduce((res, [key, val]) => ({ ...res, [getValidRoute(key)]: val }), {});
+
+      return valid_Mock;
+    } catch (err) {
+      console.log(err);
+      alert(err);
+    }
+  };
+
+  filterBySchema = (data = {}, schema = {}) => {
+    if (isPlainObject(data)) {
+      const filteredObj = Object.entries(data).reduce((result, [key, val]) => {
+        const schemaKeys = Object.keys(schema);
+        if (schemaKeys.indexOf(key) >= 0) {
+          if (isPlainObject(schema[key])) {
+            if (isPlainObject(val)) {
+              return { ...result, [key]: filterBySchema(val, schema[key]) };
+            } else if (isArray(val)) {
+              return { ...result, [key]: filterBySchema(val, schema[key]) };
+            } else {
+              return result;
+            }
+          } else if (schema[key] === true) {
+            return { ...result, [key]: val };
+          }
+          return result;
+        }
+        return result;
+      }, {});
+
+      return filteredObj;
+    } else if (isArray(data)) {
+      const filteredArray = data.map((j) => filterBySchema(j, schema)).filter((fa) => !isEmpty(fa));
+      return filteredArray.length ? filteredArray : [];
+    }
+    return data;
+  };
+
+  $(document).ready(function () {
+    const sample_schema = {
+      log: {
+        entries: {
+          _resourceType: true,
+          request: {
+            method: true,
+            url: true,
+            queryString: true,
+            postData: true,
+          },
+          response: {
+            status: true,
+            content: {
+              text: true,
+            },
+          },
+        },
+      },
     };
 
-    const filterBySchema = (data = {}, schema = {}) => {
-        if (isPlainObject(data)) {
-            const filteredObj = Object.entries(data).reduce((result, [key, val]) => {
-                const schemaKeys = Object.keys(schema);
-                if (schemaKeys.indexOf(key) >= 0) {
-                    if (isPlainObject(schema[key])) {
-                        if (isPlainObject(val)) {
-                            return {
-                                ...result,
-                                [key]: filterBySchema(val, schema[key])
-                            };
-                        } else if (isArray(val)) {
-                            return {
-                                ...result,
-                                [key]: filterBySchema(val, schema[key])
-                            };
-                        } else {
-                            return result;
-                        }
-                    } else if (schema[key] === true) {
-                        return {
-                            ...result,
-                            [key]: val
-                        };
-                    }
-                    return result;
-                }
-                return result;
-            }, {});
-
-            return filteredObj;
-        } else if (isArray(data)) {
-            const filteredArray = data.map((j) => filterBySchema(j, schema)).filter((fa) => fa);
-            return filteredArray.length ? filteredArray : [];
-        }
-        return data;
-    };
+    $("#data").val(JSON.stringify({}, null, 2));
+    $("#schema").val(JSON.stringify(sample_schema, null, 2));
 
     const setFileData = (file, id) => {
-        const fr = new FileReader();
+      const fr = new FileReader();
 
-        fr.onload = (e) => {
-            const result = JSON.parse(e.target.result);
-            var formattedData = JSON.stringify(result, null, 2);
-            $("#" + id).val(formattedData)
-        }
+      fr.onload = (e) => {
+        const result = JSON.parse(e.target.result);
+        var formattedData = JSON.stringify(result, null, 2);
+        $("#" + id).val(formattedData);
+        $("#loader-screen").removeClass("d-flex");
+        $("#loader-screen").addClass("d-none");
+      };
 
-        fr.readAsText(file);
-    }
+      fr.readAsText(file);
+    };
 
+    $(document).delegate("textarea", "keydown", function (e) {
+      var keyCode = e.keyCode || e.which;
 
-    $(document).delegate('textarea', 'keydown', function (e) {
-        var keyCode = e.keyCode || e.which;
+      if (keyCode == 9) {
+        e.preventDefault();
+        var start = selectionStart;
+        var end = selectionEnd;
 
-        if (keyCode == 9) {
-            e.preventDefault();
-            var start = this.selectionStart;
-            var end = this.selectionEnd;
+        // set textarea value to: text before caret + tab + text after caret
+        $(this).val($(this).val().substring(0, start) + "\t" + $(this).val().substring(end));
 
-            // set textarea value to: text before caret + tab + text after caret
-            $(this).val($(this).val().substring(0, start) +
-                "\t" +
-                $(this).val().substring(end));
-
-            // put caret at right position again
-            this.selectionStart =
-                this.selectionEnd = start + 1;
-        }
+        // put caret at right position again
+        selectionStart = selectionEnd = start + 1;
+      }
     });
 
-    $("#dataFile").on('change', function () {
-        const filePath = $(this).val();
-        $("#dataFileName").val(filePath);
-        const file = document.getElementById("dataFile").files[0];
-        setFileData(file, "data");
-    })
+    $("#dataFile").on("change", function () {
+      $("#loader-screen").removeClass("d-none");
+      $("#loader-screen").addClass("d-flex");
+      const filePath = $(this).val();
+      $("#dataFileName").val(filePath);
+      const file = document.getElementById("dataFile").files[0];
+      setFileData(file, "data");
+    });
 
-    $("#schemaFile").on('change', function () {
-        const filePath = $(this).val();
-        $("#schemaFileName").val(filePath);
-        const file = document.getElementById("schemaFile").files[0];
-        setFileData(file, "schema");
-    })
+    $("#schemaFile").on("change", function () {
+      const filePath = $(this).val();
+      $("#schemaFileName").val(filePath);
+      const file = document.getElementById("schemaFile").files[0];
+      setFileData(file, "schema");
+    });
 
-    $("#filterData").on('click', () => {
-        const data = $("#data").val();
-        const schema = $("#schema").val();
-        const mock = filterBySchema(JSON.parse(data), JSON.parse(schema))
-        setDownloadData(mock);
-    })
+    $("#filterData").on("click", () => {
+      const data = $("#data").val();
+      const schema = $("#schema").val();
+      const mock = filterBySchema(JSON.parse(data), JSON.parse(schema));
+      setDownloadData(mock);
+    });
 
-    $("#generateMock").on('click', () => {
-        const data = $("#data").val();
-        const mock = transformHar(JSON.parse(data), ['xhr', "document"]);
-        setDownloadData(mock);
-    })
+    $("#generateMock").on("click", () => {
+      const data = $("#data").val();
+      const mock = transformHar(JSON.parse(data), ["xhr", "document"]);
+      setDownloadData(mock);
+    });
 
     const setDownloadData = (mock) => {
-        const formattedMock = JSON.stringify(mock, null, 2);
-        $("#generatedData").val(formattedMock);
-        const dataStr = "data:text/jso;charset=utf-8," + encodeURIComponent(formattedMock);
-        $("#download").attr("href", dataStr);
-        $("#download").attr("download", "mock.json");
-        $("#download").removeClass("d-none");
-        $("#download").addClass("d-inline-block");
-    }
-});
+      const formattedMock = JSON.stringify(mock, null, 2);
+      $("#generatedData").val(formattedMock);
+      const dataStr = "data:text/jso;charset=utf-8," + encodeURIComponent(formattedMock);
+      $("#download").attr("href", dataStr);
+      $("#download").attr("download", "mock.json");
+      $("#download").removeClass("d-none");
+      $("#download").addClass("d-inline-block");
+    };
+  });
+} catch (err) {
+  console.log(err);
+  alert(err);
+}
